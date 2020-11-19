c674df9ca263fb35bd8fe996bbf31e59a31f273e
Yaser Afshar <ya.afshar@gmail.com>
2020-11-19 13:36:59 -0600

    Update the utils module, adding extra functionality for subsampling from the time series data

--- convergence/utils.py
+++ convergence/utils.py
@@ -1,8 +1,10 @@
 """Utility module."""
 
 import numpy as np
+from random import randint
 
 from .err import CVGError
+from .batch import batch
 from .statistical_inefficiency import \
     statistical_inefficiency, \
     r_statistical_inefficiency, \
@@ -14,10 +16,13 @@ __all__ = [
     'validate_split',
     'train_test_split',
     'subsample',
+    'subsample_index',
+    'random_subsample',
+    'block_average_subsample',
 ]
 
 
-def validate_split(n_samples, train_size, test_size, default_test_size=None):
+def validate_split(*, n_samples, train_size, test_size, default_test_size=None):
     r"""Validate test/train sizes.
 
     Helper function to validate the test/train sizes to be meaningful with
@@ -55,11 +60,13 @@ def validate_split(n_samples, train_size, test_size, default_test_size=None):
         msg = 'test_size={} should be positive '.format(test_size)
         msg += 'and smaller than the number of samples={}'.format(n_samples)
         raise CVGError(msg)
-    elif test_size_float and (test_size <= 0 or test_size >= 1):
+    
+    if test_size_float and (test_size <= 0 or test_size >= 1):
         msg = 'test_size={} should be a float in '.format(test_size)
         msg += 'the [0, 1] range.'
         raise CVGError(msg)
-    elif test_size is not None:
+    
+    if test_size is not None:
         msg = 'Invalid input of test_size={}.'.format(test_size)
         raise CVGError(msg)
 
@@ -67,11 +74,13 @@ def validate_split(n_samples, train_size, test_size, default_test_size=None):
         msg = 'train_size={} should be positive '.format(train_size)
         msg += 'and smaller than the number of samples={}'.format(n_samples)
         raise CVGError(msg)
-    elif train_size_float and (train_size <= 0 or train_size >= 1):
+
+    if train_size_float and (train_size <= 0 or train_size >= 1):
         msg = 'train_size={} should be a float '.format(test_size)
         msg += 'in the [0, 1] range.'
         raise CVGError(msg)
-    elif train_size is not None:
+
+    if train_size is not None:
         msg = 'Invalid input of train_size={}.'.format(train_size)
         raise CVGError(msg)
 
@@ -112,11 +121,15 @@ def validate_split(n_samples, train_size, test_size, default_test_size=None):
     return n_train, n_test
 
 
-def train_test_split(x, *, test_size=None, train_size=None, seed=None, default_test_size=0.1):
-    r"""Split x into random train and test indices.
+def train_test_split(time_series_data, *,
+                     train_size=None,
+                     test_size=None,
+                     seed=None,
+                     default_test_size=0.1):
+    r"""Split time_series_data into random train and test indices.
 
     Args:
-        x (array_like): time series data, array-like of shape
+        time_series_data (array_like): time series data, array-like of shape
             ``(n_samples, n_features)``, where n_samples is the number of
             samples and n_features is the number of features.
         test_size (int, float, or None, optional): if ``float``, should be
@@ -138,11 +151,11 @@ def train_test_split(x, *, test_size=None, train_size=None, seed=None, default_t
         1darray, 1darray: training indices, testing indices.
 
     """
-    x = np.array(x, copy=False)
-    n_samples = np.shape(x)[0]
-    n_train, n_test = validate_split(n_samples,
-                                     test_size,
-                                     train_size,
+    time_series_data = np.array(time_series_data, copy=False)
+    n_samples = np.shape(time_series_data)[0]
+    n_train, n_test = validate_split(n_samples=n_samples,
+                                     train_size=train_size,
+                                     test_size=test_size,
                                      default_test_size=default_test_size)
 
     if seed is None or isinstance(seed, int):
@@ -161,32 +174,43 @@ def train_test_split(x, *, test_size=None, train_size=None, seed=None, default_t
     return ind_train, ind_test
 
 
-def subsample(x, *, si=None, fft=False, mct=5):
-    r"""Sample the uncorrelated subsample of the time series data.
+def subsample_index(time_series_data, *,
+                    si=None,
+                    fft=False,
+                    minimum_correlation_time=5):
+    r"""Return indices of uncorrelated subsamples of the time series data.
 
+    Return indices of the uncorrelated subsample of the time series data.
     Subsample a correlated timeseries to extract an effectively uncorrelated
     dataset. If si (statistical inefficiency) is not provided it will be
     computed.
 
     Args:
-        x (array_like, 1d): time series data.
+        time_series_data (array_like, 1d): time series data.
         si (float, or str, optional): estimated statistical inefficiency.
             (default: None)
         fft (bool): if True, use FFT convolution. FFT should be preferred
             for long time series. (default: False)
-        mct (int, optional): minimum amount of correlation function to compute.
-            The algorithm terminates after computing the correlation time out
-            to mct when the correlation function first goes negative.
-            (default: 5)
+        minimum_correlation_time (int, optional): minimum amount of correlation 
+            function to compute. The algorithm terminates after computing the 
+            correlation time out to minimum_correlation_time when the 
+            correlation function first goes negative. (default: 5)
 
     Returns:
-        list: indices list.
-            Indices list of an uncorrelated subsample of the time series data.
+        1darray: indices array.
+            Indices of uncorrelated subsamples of the time series data.
 
     """
     if si is None:
-        # Compute the statistical inefficiency for the timeseries
-        si = statistical_inefficiency(x, fft=fft, mct=mct)
+        try:
+            # Compute the statistical inefficiency for the timeseries
+            si = statistical_inefficiency(
+                time_series_data, fft=fft,
+                minimum_correlation_time=minimum_correlation_time)
+        except CVGError:
+            msg = 'Failed to compute the statistical inefficiency for the '
+            msg += 'time_series_data'
+            raise CVGError(msg)
     else:
         if isinstance(si, str):
             if si not in si_methods:
@@ -196,9 +220,17 @@ def subsample(x, *, si=None, fft=False, mct=5):
                 raise CVGError(msg)
 
             si_func = si_methods[si]
-            si = si_func(x, fft=fft, mct=mct)
-        elif isinstance(si, float) or isinstance(si, int):
-            if (si < 1.0):
+
+            try:
+                si = si_func(
+                    time_series_data, fft=fft,
+                    minimum_correlation_time=minimum_correlation_time)
+            except CVGError:
+                msg = 'Failed to compute the statistical inefficiency for the '
+                msg += 'time_series_data'
+                raise CVGError(msg)
+        elif isinstance(si, (float, int)):
+            if si < 1.0:
                 msg = 'statistical inefficiency = {} must be '.format(si)
                 msg += 'greater than or equal one.'
                 raise CVGError(msg)
@@ -206,13 +238,181 @@ def subsample(x, *, si=None, fft=False, mct=5):
             msg = 'statistical inefficiency (si) must be a `float` or `str`.'
             raise CVGError(msg)
 
-    # Get the length of the timeseries
-    n = len(x)
+    # Get the length of the time_series_data
+    n = len(time_series_data)
 
-    _ind = si * np.arange(0., n)
+    _ind = si * np.arange(0, n)
+
+    # Each block should contain more steps than si
+    _ind = np.ceil(_ind).astype(int)
+    _ind_indices = np.where(_ind < n)
 
     # Assemble list of indices of uncorrelated snapshots.
-    indices = np.where(_ind.astype(int) < n)
+    indices = _ind[_ind_indices[0]]
 
     # Return the list of indices of uncorrelated snapshots.
     return indices
+
+
+def subsample(time_series_data, *,
+              si=None,
+              fft=False,
+              minimum_correlation_time=5):
+    r"""Return time series data at uncorrelated subsample indices.
+
+    Subsample a correlated timeseries to extract an effectively uncorrelated
+    dataset. If si (statistical inefficiency) is not provided it will be
+    computed.
+
+    Args:
+        time_series_data (array_like, 1d): time series data.
+        si (float, or str, optional): estimated statistical inefficiency.
+            (default: None)
+        fft (bool): if True, use FFT convolution. FFT should be preferred
+            for long time series. (default: False)
+        minimum_correlation_time (int, optional): minimum amount of correlation 
+            function to compute. The algorithm terminates after computing the 
+            correlation time out to minimum_correlation_time when the 
+            correlation function first goes negative. (default: 5)
+
+    Returns:
+        1darray: subsample of the time series data.
+            time series data at uncorrelated subsample indices.
+
+    """
+    time_series_data = np.array(time_series_data, copy=False)
+
+    # Check inputs
+    if time_series_data.ndim != 1:
+        msg = 'time_series_data is not an array of one-dimension.'
+        raise CVGError(msg)
+
+    try:
+        indices = subsample_index(
+            time_series_data=time_series_data,
+            si=si,
+            fft=fft,
+            minimum_correlation_time=minimum_correlation_time)
+    except CVGError:
+        msg = 'Failed to compute the indices of uncorrelated subsamples of '
+        msg += 'the time_series_data.'
+        raise CVGError(msg)
+
+    return time_series_data[indices]
+
+
+def random_subsample(time_series_data, *,
+                     si=None,
+                     fft=False,
+                     minimum_correlation_time=5):
+    r"""Retuen random data for each block after blocking the time series data.
+
+    At first, break down the time series data into the series of blocks, where
+    each block contains ``si`` successive data points. If si
+    (statistical inefficiency) is not provided it will be computed. Then a
+    single value is taken at random from each block.
+
+    Args:
+        time_series_data (array_like, 1d): time series data.
+        si (float, or str, optional): estimated statistical inefficiency.
+            (default: None)
+        fft (bool): if True, use FFT convolution. FFT should be preferred
+            for long time series. (default: False)
+        minimum_correlation_time (int, optional): minimum amount of correlation 
+            function to compute. The algorithm terminates after computing the 
+            correlation time out to minimum_correlation_time when the 
+            correlation function first goes negative. (default: 5)
+
+    Returns:
+        1darray: subsample of the time series data.
+            random data for each block after blocking the time series data.
+
+    """
+    time_series_data = np.array(time_series_data, copy=False)
+
+    # Check inputs
+    if time_series_data.ndim != 1:
+        msg = 'time_series_data is not an array of one-dimension.'
+        raise CVGError(msg)
+
+    try:
+        indices = subsample_index(
+            time_series_data=time_series_data,
+            si=si,
+            fft=fft,
+            minimum_correlation_time=minimum_correlation_time)
+    except CVGError:
+        msg = 'Failed to compute the indices of uncorrelated subsamples of '
+        msg += 'the time_series_data.'
+        raise CVGError(msg)
+
+    random_subsample_data = np.empty(indices.size,
+                                     dtype=time_series_data.dtype)
+
+    index_s = 0
+    for index, index_e in enumerate(indices[1:-1]):
+        rand_index = randint(index_s, index_e - 1)
+        random_subsample_data[index] = time_series_data[rand_index]
+        index_s = index_e
+    
+    rand_index = randint(index_s, indices[-1])
+    random_subsample_data[-1] = time_series_data[rand_index]
+
+    return random_subsample_data
+
+
+def block_average_subsample(time_series_data, *,
+                            si=None,
+                            fft=False,
+                            minimum_correlation_time=5):
+    """Retuen average value for each block after blocking the time series data.
+
+    At first, break down the time series data into the series of blocks, where
+    each block contains ``si`` successive data points. If si
+    (statistical inefficiency) is not provided it will be computed. Then the
+    average value for each block is determined. This coarse graining approach is
+    commonly used for thermodynamic properties.
+
+    Args:
+        time_series_data (array_like, 1d): time series data.
+        si (float, or str, optional): estimated statistical inefficiency.
+            (default: None)
+        fft (bool): if True, use FFT convolution. FFT should be preferred
+            for long time series. (default: False)
+        minimum_correlation_time (int, optional): minimum amount of correlation 
+            function to compute. The algorithm terminates after computing the 
+            correlation time out to minimum_correlation_time when the 
+            correlation function first goes negative. (default: 5)
+
+    Returns:
+        1darray: subsample of the time series data.
+            average value for each block after blocking the time series data.
+
+    """
+    time_series_data = np.array(time_series_data, copy=False)
+
+    # Check inputs
+    if time_series_data.ndim != 1:
+        msg = 'time_series_data is not an array of one-dimension.'
+        raise CVGError(msg)
+
+    try:
+        indices = subsample_index(
+            time_series_data=time_series_data,
+            si=si,
+            fft=fft,
+            minimum_correlation_time=minimum_correlation_time)
+    except CVGError:
+        msg = 'Failed to compute the indices of uncorrelated subsamples of '
+        msg += 'the time_series_data.'
+        raise CVGError(msg)
+
+    mean_subsample_data = np.empty(indices.size - 1,
+                                   dtype=time_series_data.dtype)
+
+    index_s = 0
+    for index, index_e in enumerate(indices[1:]):
+        mean_subsample_data[index] = np.mean(time_series_data[index_s:index_e])
+        index_s = index_e
+
+    return mean_subsample_data
