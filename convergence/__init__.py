a4b1644a81fe2b5d9f97b63f9912625f9118a386
Yaser Afshar <ya.afshar@gmail.com>
2021-05-06 16:11:56 -0500

    update the init module

--- convergence/__init__.py
+++ convergence/__init__.py
@@ -2,13 +2,31 @@ r"""Convergence package."""
 
 from .err import CVGError, cvg_warning
 from .stats import \
+    beta, \
+    betacf, \
+    betai, \
+    betai_cdf_ccdf, \
+    betai_cdf, \
+    randomness_test, \
+    s_normal_inv_cdf, \
+    normal_inv_cdf, \
+    normal_interval, \
     get_fft_optimal_size, \
     auto_covariance, \
     auto_correlate, \
     cross_covariance, \
     cross_correlate, \
     modified_periodogram, \
-    periodogram
+    periodogram, \
+    int_power, \
+    moment, \
+    skew, \
+    t_cdf_ccdf, \
+    t_cdf, \
+    t_inv_cdf, \
+    t_interval, \
+    ZERO_RC_BOUNDS, \
+    ZERO_RC
 from .outlier import \
     outlier_methods, \
     outlier_test
@@ -27,8 +45,6 @@ from .scale import \
 from .batch import batch
 from .mser_m import mser_m
 #from .geweke import geweke
-from .s_normal_dist import s_normal_inv_cdf
-from .t_dist import t_inv_cdf, t_interval
 from .statistical_inefficiency import \
     statistical_inefficiency, \
     r_statistical_inefficiency, \
@@ -39,22 +55,41 @@ from .statistical_inefficiency import \
 from .utils import \
     validate_split, \
     train_test_split, \
-    subsample, \
-    subsample_index, \
-    random_subsample, \
-    block_average_subsample
+    time_series_data_si, \
+    uncorrelated_time_series_data_sample_indices, \
+    uncorrelated_time_series_data_samples, \
+    time_series_data_uncorrelated_samples, \
+    time_series_data_uncorrelated_random_samples, \
+    time_series_data_uncorrelated_block_averaged_samples
 from .equilibration_length import estimate_equilibration_length
 from .ucl import \
     HeidelbergerWelch, \
-    ucl, \
-    subsamples_ucl, \
-    ucl_methods
+    heidelberger_welch_ucl, \
+    heidelberger_welch_ci, \
+    heidelberger_welch_relative_half_width_estimate, \
+    UncorrelatedSamples, \
+    uncorrelated_samples_ucl, \
+    uncorrelated_samples_ci, \
+    uncorrelated_samples_relative_half_width_estimate, \
+    N_SKART, \
+    n_skart_ucl, \
+    n_skart_ci, \
+    n_skart_relative_half_width_estimate
 from .timeseries import \
     run_length_control
 
 __all__ = [
     'CVGError',
     'cvg_warning',
+    'beta',
+    'betacf',
+    'betai',
+    'betai_cdf_ccdf',
+    'betai_cdf',
+    'randomness_test',
+    's_normal_inv_cdf',
+    'normal_inv_cdf',
+    'normal_interval',
     'get_fft_optimal_size',
     'auto_covariance',
     'auto_correlate',
@@ -62,6 +97,15 @@ __all__ = [
     'cross_correlate',
     'modified_periodogram',
     'periodogram',
+    'int_power',
+    'moment',
+    'skew',
+    't_cdf_ccdf',
+    't_cdf',
+    't_inv_cdf',
+    't_interval',
+    'ZERO_RC_BOUNDS',
+    'ZERO_RC',
     'outlier_methods',
     'outlier_test',
     'MinMaxScale',
@@ -78,9 +122,6 @@ __all__ = [
     'batch',
     # 'geweke',
     'mser_m',
-    's_normal_inv_cdf',
-    't_inv_cdf',
-    't_interval',
     'statistical_inefficiency',
     'r_statistical_inefficiency',
     'split_r_statistical_inefficiency',
@@ -89,14 +130,25 @@ __all__ = [
     'integrated_auto_correlation_time',
     'validate_split',
     'train_test_split',
-    'subsample',
-    'subsample_index',
-    'random_subsample',
-    'block_average_subsample',
+    'time_series_data_si',
+    'uncorrelated_time_series_data_sample_indices',
+    'uncorrelated_time_series_data_samples',
+    'time_series_data_uncorrelated_samples',
+    'time_series_data_uncorrelated_random_samples',
+    'time_series_data_uncorrelated_block_averaged_samples',
     'estimate_equilibration_length',
     'HeidelbergerWelch',
-    'ucl',
-    'subsamples_ucl',
+    'heidelberger_welch_ucl',
+    'heidelberger_welch_ci',
+    'heidelberger_welch_relative_half_width_estimate',
+    'UncorrelatedSamples',
+    'uncorrelated_samples_ucl',
+    'uncorrelated_samples_ci',
+    'uncorrelated_samples_relative_half_width_estimate',
+    'N_SKART',
+    'n_skart_ucl',
+    'n_skart_ci',
+    'n_skart_relative_half_width_estimate',
     'ucl_methods',
     'run_length_control',
 ]
