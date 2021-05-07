8a42006e225e6deb7a08c9580f8538f6fb0dd2c1
Yaser Afshar <ya.afshar@gmail.com>
2021-05-06 19:08:15 -0500

    update the file permissions

--- convergence/timeseries/__init__.py
+++ convergence/timeseries/__init__.py
@@ -1,5 +1,8 @@
 """Time series module."""
 
+from .equilibration_length import \
+    estimate_equilibration_length
+
 from .run_length_control import \
     run_length_control
 
@@ -12,6 +15,7 @@ from .utils import \
     time_series_data_uncorrelated_block_averaged_samples
 
 __all__ = [
+    'estimate_equilibration_length'
     'run_length_control',
     'time_series_data_si',
     'uncorrelated_time_series_data_sample_indices',
