08ee31060b5d9668be0a7b4a0fc1f113f266889f
Yaser Afshar <ya.afshar@gmail.com>
2021-05-14 14:07:27 -0500

    update the init

--- convergence/timeseries/__init__.py
+++ convergence/timeseries/__init__.py
@@ -3,8 +3,16 @@
 from .equilibration_length import \
     estimate_equilibration_length
 
-from .run_length_control import \
-    run_length_control
+# from .run_length_control import \
+#     run_length_control
+
+from .statistical_inefficiency import \
+    statistical_inefficiency, \
+    r_statistical_inefficiency, \
+    split_r_statistical_inefficiency,\
+    split_statistical_inefficiency, \
+    si_methods, \
+    integrated_auto_correlation_time
 
 from .utils import \
     time_series_data_si, \
@@ -15,8 +23,14 @@ from .utils import \
     time_series_data_uncorrelated_block_averaged_samples
 
 __all__ = [
-    'estimate_equilibration_length'
-    'run_length_control',
+    'estimate_equilibration_length',
+    # 'run_length_control',
+    'statistical_inefficiency',
+    'r_statistical_inefficiency',
+    'split_r_statistical_inefficiency',
+    'split_statistical_inefficiency',
+    'si_methods',
+    'integrated_auto_correlation_time',
     'time_series_data_si',
     'uncorrelated_time_series_data_sample_indices',
     'uncorrelated_time_series_data_samples',
