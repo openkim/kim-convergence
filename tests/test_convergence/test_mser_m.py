c456431cdc29bfabc2206ca196291a640f0e416a
Yaser Afshar <ya.afshar@gmail.com>
2021-05-17 13:59:12 -0500

    update the UCL mser_m test

--- tests/test_convergence/test_mser_m.py
+++ tests/test_convergence/test_mser_m.py
@@ -1,4 +1,4 @@
-"""Test mser_m module."""
+"""Test UCL mser_m module."""
 import unittest
 import numpy as np
 
@@ -10,8 +10,8 @@ except:
 from convergence import CVGError
 
 
-class TestMSERModule(unittest.TestCase):
-    """Test mser_m module components."""
+class TestUCLMSERModule(unittest.TestCase):
+    """Test UCL mser_m module components."""
 
     def test_mser_m(self):
         """Test mser_m function."""
@@ -45,55 +45,69 @@ class TestMSERModule(unittest.TestCase):
 
         self.assertRaises(CVGError, cr.mser_m, x)
 
+        # const value array less than batch_size
         # one value array
         n = 1
         x = np.ones(n) * 10
         truncated, truncated_i = cr.mser_m(x)
         self.assertFalse(truncated)
+        self.assertEqual(truncated_i, 0)
 
-        # const value array less than batch_size
         n = 4
         x = np.ones(n)
         truncated, truncated_i = cr.mser_m(x)
         self.assertFalse(truncated)
+        self.assertEqual(truncated_i, 0)
+
+        rng = np.random.RandomState(12345)
 
         n = 100
-        x = np.ones(n) * 10 + (np.random.random_sample(n) - 0.5)
+        x = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
 
-        # invalid int ignore_end_batch
+        # invalid int ignore_end
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=0)
+                          x, ignore_end=0)
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=-1)
+                          x, ignore_end=-1)
 
-        # invalid float ignore_end_batch
+        # invalid float ignore_end
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=0.0)
+                          x, ignore_end=0.0)
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=1.0)
+                          x, ignore_end=1.0)
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=-0.1)
+                          x, ignore_end=-0.1)
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=1.1)
+                          x, ignore_end=1.1)
 
-        # invalid ignore_end_batch
+        # invalid ignore_end
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch="None")
+                          x, ignore_end="None")
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch="1")
+                          x, ignore_end="1")
 
-        # invalid ignore_end_batch
+        # invalid ignore_end
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=120)
+                          x, ignore_end=120)
         self.assertRaises(CVGError, cr.mser_m,
-                          x, ignore_end_batch=100)
+                          x, ignore_end=100)
 
         # Create synthetic data
+        rng = np.random.RandomState(12345)
+
         n = 1000
         x = np.arange(10.)
-        _x = np.ones(n) * 10 + (np.random.random_sample(n) - 0.5)
-        x = np.concatenate((x, _x))
+        y = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
+        x = np.concatenate((x, y))
 
         truncated, truncated_i = cr.mser_m(x)
         self.assertTrue(truncated)
         self.assertTrue(truncated_i >= 10)
+
+        x = np.arange(20, 10, -1)
+        x = np.concatenate((x, y))
+        truncated, truncated_i = cr.mser_m(x)
+
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i >= 10)
+
