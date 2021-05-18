7be8b43e2b4407371a4d423679d6c23fdfbedfef
Yaser Afshar <ya.afshar@gmail.com>
2021-05-18 13:56:09 -0500

    update the batch test with extra cases

--- tests/test_convergence/test_batch.py
+++ tests/test_convergence/test_batch.py
@@ -1,6 +1,7 @@
 """Test batch module."""
 import unittest
 import numpy as np
+from numpy.lib.function_base import place
 
 try:
     import convergence as cr
@@ -29,101 +30,130 @@ class TestBatchModule(unittest.TestCase):
 
         # scale method
         self.assertRaises(CVGError, cr.batch, x,
-                          batch_size=5, scale=cr.translate_scale)
+                          batch_size=5, scale=cr.translate_scale,
+                          with_centering=True)
         self.assertRaises(CVGError, cr.batch, x,
-                          batch_size=5, scale='new_scale_metod')
+                          batch_size=5, scale='new_scale_metod',
+                          with_centering=True)
         self.assertRaises(CVGError, cr.batch, x,
-                          batch_size=5, scale=None)
+                          batch_size=5, scale=None,
+                          with_centering=True)
 
         # n_batches
+        rng = np.random.RandomState(12345)
+
         n = 100
-        x = np.ones(n) * 10 + (np.random.random_sample(n) - 0.5)
+        x = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
         self.assertRaises(CVGError, cr.batch, x, batch_size=101)
 
         x = np.ones(n)
         b = cr.batch(x,
-                      batch_size=5,
-                      with_centering=False,
-                      with_scaling=False)
+                     batch_size=5,
+                     with_centering=False,
+                     with_scaling=False)
         for i in b:
             self.assertTrue(i == 1.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      with_centering=False,
-                      with_scaling=True)
+                     batch_size=5,
+                     with_centering=False,
+                     with_scaling=True)
         for i in b:
             self.assertTrue(i == 1.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      with_centering=True,
-                      with_scaling=True)
+                     batch_size=5,
+                     with_centering=True,
+                     with_scaling=True)
         for i in b:
             self.assertTrue(i == 0.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      with_centering=True,
-                      with_scaling=False)
+                     batch_size=5,
+                     with_centering=True,
+                     with_scaling=False)
         for i in b:
             self.assertTrue(i == 0.0)
 
         b = cr.batch(x,
-                      batch_size=7,
-                      with_centering=False,
-                      with_scaling=False)
+                     batch_size=7,
+                     with_centering=False,
+                     with_scaling=False)
         for i in b:
             self.assertTrue(i == 1.0)
         b = cr.batch(x,
-                      batch_size=7,
-                      with_centering=False,
-                      with_scaling=True)
-        for i in b:
-            self.assertTrue(i == 1.0)
-
-        b = cr.batch(x,
-                      batch_size=7,
-                      with_centering=True,
-                      with_scaling=True)
-        for i in b:
-            self.assertTrue(i == 0.0)
-
-        b = cr.batch(x,
-                      batch_size=7,
-                      with_centering=True,
-                      with_scaling=False)
-        for i in b:
-            self.assertTrue(i == 0.0)
-
-        b = cr.batch(x,
-                      batch_size=5,
-                      scale='translate_scale',
-                      with_centering=False,
-                      with_scaling=False)
+                     batch_size=7,
+                     with_centering=False,
+                     with_scaling=True)
         for i in b:
             self.assertTrue(i == 1.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      scale='translate_scale',
-                      with_centering=False,
-                      with_scaling=True)
+                     batch_size=7,
+                     with_centering=True,
+                     with_scaling=True)
+        for i in b:
+            self.assertTrue(i == 0.0)
+
+        b = cr.batch(x,
+                     batch_size=7,
+                     with_centering=True,
+                     with_scaling=False)
+        for i in b:
+            self.assertTrue(i == 0.0)
+
+        b = cr.batch(x,
+                     batch_size=5,
+                     scale='translate_scale',
+                     with_centering=False,
+                     with_scaling=False)
         for i in b:
             self.assertTrue(i == 1.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      scale='translate_scale',
-                      with_centering=True,
-                      with_scaling=True)
+                     batch_size=5,
+                     scale='translate_scale',
+                     with_centering=False,
+                     with_scaling=True)
+        for i in b:
+            self.assertTrue(i == 1.0)
+
+        b = cr.batch(x,
+                     batch_size=5,
+                     scale='translate_scale',
+                     with_centering=True,
+                     with_scaling=True)
         for i in b:
             self.assertTrue(i == 0.0)
 
         b = cr.batch(x,
-                      batch_size=5,
-                      scale='translate_scale',
-                      with_centering=True,
-                      with_scaling=False)
+                     batch_size=5,
+                     scale='translate_scale',
+                     with_centering=True,
+                     with_scaling=False)
         for i in b:
             self.assertTrue(i == 0.0)
+
+        rng = np.random.RandomState(12345)
+
+        n = 1000
+        x = np.ones(n) * 100 + (rng.random_sample(n) - 0.5)
+
+        b = cr.batch(x)
+
+        self.assertEqual(b.size, n // 5)
+        self.assertAlmostEqual(b.mean(), 100.0, places=2)
+
+        # test median as a reduction function
+
+        test_passed = True
+
+        try:
+            b = cr.batch(x, func=np.median)
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+
+        self.assertEqual(b.size, n // 5)
+        self.assertAlmostEqual(b.mean(), 100.0, places=1)
