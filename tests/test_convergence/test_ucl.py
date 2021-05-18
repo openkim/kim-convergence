4b593a44b8ad3ce0a2bfb3c26439b9ee6fd80c28
Yaser Afshar <ya.afshar@gmail.com>
2021-05-18 16:57:39 -0500

    add extra test cases

--- tests/test_convergence/test_ucl.py
+++ tests/test_convergence/test_ucl.py
@@ -1,6 +1,8 @@
 """Test ucl module."""
+from os import truncate
 import unittest
 import numpy as np
+from numpy.lib.function_base import select
 from numpy.testing._private.utils import assert_equal
 
 try:
@@ -13,6 +15,59 @@ from convergence import CVGError
 
 class TestUCLModule(unittest.TestCase):
     """Test ucl module components."""
+
+    def test_ucl_base(self):
+        """Test Upper Confidence Limit base class."""
+        test_passed = True
+
+        try:
+            # Initialize the HeidelbergerWelch object
+            ucl_base = cr.ucl.UCLBase()
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+
+        self.assertIsNone(ucl_base.mean)
+        self.assertIsNone(ucl_base.std)
+        self.assertIsNone(ucl_base.si)
+        self.assertIsNone(ucl_base.indices)
+
+        rng = np.random.RandomState(12345)
+        n = 1000
+        x = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
+        y = np.concatenate(
+            (np.arange(n // 10)[::-1] / float(n // 10),
+             np.zeros(n - n // 10)))
+
+        x += y
+
+        test_passed = True
+
+        try:
+            truncated, truncated_index = \
+                ucl_base.estimate_equilibration_length(x)
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertTrue(truncated)
+        self.assertEqual(ucl_base.si, 1.0)
+
+        x_cut = x[truncated_index:]
+
+        try:
+            ucl_base.set_indices(x_cut)
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertEqual(x_cut.size, ucl_base.indices.size)
+
+        ucl = ucl_base.ucl(x_cut)
+        self.assertEqual(ucl, 1e100)
+
+        self.assertRaises(TypeError, ucl_base.ci, x_cut)
 
     def test_set_heidel_welch_constants(self):
         """Test set_heidel_welch_constants function."""
@@ -243,8 +298,7 @@ class TestUCLModule(unittest.TestCase):
 
         x = np.arange(100.)
 
-        self.assertRaises(CVGError, heidel_welch.ucl,
-                          x.reshape(5, 20))
+        self.assertRaises(CVGError, heidel_welch.ucl, x.reshape(5, 20))
 
         # input data points are not sufficient
         x = np.arange(10.)
@@ -254,6 +308,19 @@ class TestUCLModule(unittest.TestCase):
         rng = np.random.RandomState(12345)
 
         x = np.ones(1000) * 10 + (rng.random_sample(1000) - 0.5)
+
+        test_passed = True
+
+        try:
+            truncated, truncated_index = \
+                heidel_welch.estimate_equilibration_length(x)
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertTrue(truncated)
+        self.assertEqual(truncated_index, 0)
+        self.assertEqual(heidel_welch.si, 1.0)
 
         test_passed = True
 
@@ -273,10 +340,8 @@ class TestUCLModule(unittest.TestCase):
 
         self.assertTrue(test_passed)
 
-        self.assertEqual(heidel_welch.mean - upper_confidence_limit_1,
-                         lower)
-        self.assertEqual(heidel_welch.mean + upper_confidence_limit_1,
-                         upper)
+        self.assertEqual(heidel_welch.mean - upper_confidence_limit_1, lower)
+        self.assertEqual(heidel_welch.mean + upper_confidence_limit_1, upper)
 
         try:
             relative_half_width_estimate = \
@@ -347,44 +412,240 @@ class TestUCLModule(unittest.TestCase):
         except CVGError:
             test_passed = False
 
-        # self.assertTrue(test_passed)
+        self.assertTrue(test_passed)
 
-        # self.assertIsNone(usamples.mean)
-        # self.assertIsNone(usamples.std)
-        # self.assertIsNone(usamples.si)
-        # self.assertIsNone(usamples.indices)
+        self.assertIsNone(usamples.mean)
+        self.assertIsNone(usamples.std)
+        self.assertIsNone(usamples.si)
+        self.assertIsNone(usamples.indices)
 
-        # x = np.arange(100.)
+        x = np.arange(100.)
 
-        # self.assertRaises(CVGError, usamples.ucl, x.reshape(5, 20))
+        self.assertRaises(CVGError, usamples.ucl, x.reshape(5, 20))
 
-        # self.assertRaises(CVGError, usamples.ucl, x, confidence_coefficient=0)
-        # self.assertRaises(CVGError, usamples.ucl, x, confidence_coefficient=1)
+        self.assertRaises(CVGError, usamples.ucl, x, confidence_coefficient=0)
+        self.assertRaises(CVGError, usamples.ucl, x, confidence_coefficient=1)
 
-        # # input data points are not sufficient
-        # x = np.arange(4.)
+        # input data points are not sufficient
+        x = np.arange(4.)
 
-        # self.assertRaises(CVGError, usamples.ucl, x)
+        self.assertRaises(CVGError, usamples.ucl, x)
 
         rng = np.random.RandomState(12345)
 
         x = np.ones(1000) * 10 + (rng.random_sample(1000) - 0.5)
 
-        # test_passed = True
+        test_passed = True
 
-        # try:
-        #     upper_confidence_limit_1 = usamples.ucl(x)
-        # except CVGError:
-        #     test_passed = False
+        try:
+            usamples.ucl(x)
+        except CVGError:
+            test_passed = False
 
-        # self.assertTrue(test_passed)
+        self.assertTrue(test_passed)
 
-        # self.assertAlmostEqual(usamples.mean, x.mean(), places=15)
-        # self.assertAlmostEqual(usamples.std, x.std(), places=15)
-        # self.assertTrue(np.size(usamples.indices) == 1000)
+        self.assertAlmostEqual(usamples.mean, x.mean())
+        self.assertAlmostEqual(usamples.std, x.std())
+        self.assertTrue(np.size(usamples.indices) == 1000)
 
+        y = np.array((x[0], x[0], x[0], x[0], x[0], x[0], x[0], x[0]))
 
-        y = np.array((x[0], x[0], x[0], x[0], x[0]))
-        # self.assertRaises(CVGError, usamples.ucl, x[0:1])
-        # print(usamples.ucl(y))
+        try:
+            usamples.ucl(y)
+        except CVGError:
+            test_passed = False
 
+        self.assertFalse(test_passed)
+
+        test_passed = True
+
+        indices = np.arange(y.size)
+
+        try:
+            ucl = usamples.ucl(y, uncorrelated_sample_indices=indices)
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+
+        self.assertAlmostEqual(ucl, 0)
+
+        self.assertAlmostEqual(usamples.mean, x[0])
+        self.assertAlmostEqual(usamples.std, 0)
+        self.assertTrue(np.all(indices == usamples.indices))
+
+    def test_MSER_m(self):
+        """Test MSER_m class."""
+        mser = cr.MSER_m()
+
+        self.assertIsNone(mser.indices)
+        self.assertIsNone(mser.si)
+        self.assertIsNone(mser.mean)
+        self.assertIsNone(mser.std)
+
+        n = 100
+        x = np.ones(n)
+
+        # x is not one dimensional
+        self.assertRaises(CVGError, mser.ucl, x.reshape(5, 20))
+
+        # nan in the input
+        if n > 10:
+            x[10] = np.nan
+
+        self.assertRaises(CVGError, mser.ucl, x)
+
+        rng = np.random.RandomState(12345)
+
+        n = 1000
+        x = np.arange(10.)
+        y = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
+        x = np.concatenate((x, y))
+
+        truncated, truncated_i = mser.estimate_equilibration_length(x)
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i >= 10)
+        self.assertAlmostEqual(mser.si, 1.0)
+
+        test_passed = True
+
+        try:
+            mser.ucl(x[truncated_i:])
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+
+        self.assertAlmostEqual(mser.mean, 10, places=2)
+        self.assertTrue(mser.std < np.std(x[truncated_i:]))
+
+        rng = np.random.RandomState(12345)
+
+        n = 4000
+        x = np.arange(0, 10, 10. / (n / 4))
+        x += (rng.random_sample(x.size) - 0.5)
+        y = np.ones(n) * 10 + rng.randn(n)
+        x = np.concatenate((x, y))
+
+        truncated, truncated_i = mser.estimate_equilibration_length(x)
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i < n // 2)
+
+        test_passed = True
+
+        try:
+            ucl = mser.ucl(x[truncated_i:])
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertAlmostEqual(mser.mean, 10, places=1)
+
+    def test_MSER_m_y(self):
+        """Test MSER_m_y class."""
+        mser = cr.MSER_m_y()
+
+        self.assertIsNone(mser.indices)
+        self.assertIsNone(mser.si)
+        self.assertIsNone(mser.mean)
+        self.assertIsNone(mser.std)
+
+        n = 100
+        x = np.ones(n)
+
+        # x is not one dimensional
+        self.assertRaises(CVGError, mser.ucl, x.reshape(5, 20))
+
+        # nan in the input
+        if n > 10:
+            x[10] = np.nan
+
+        self.assertRaises(CVGError, mser.ucl, x)
+
+        rng = np.random.RandomState(12345)
+
+        n = 1000
+        x = np.arange(10.)
+        y = np.ones(n) * 10 + (rng.random_sample(n) - 0.5)
+        x = np.concatenate((x, y))
+
+        truncated, truncated_i = mser.estimate_equilibration_length(x)
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i >= 10)
+        self.assertAlmostEqual(mser.si, 1.0)
+
+        test_passed = True
+
+        try:
+            mser.ucl(x[truncated_i:])
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+
+        self.assertAlmostEqual(mser.mean, 10, places=2)
+        self.assertAlmostEqual(mser.std, np.std(x[truncated_i:]))
+
+        rng = np.random.RandomState(12345)
+
+        n = 4000
+        x = np.arange(0, 10, 10. / (n / 4))
+        x += (rng.random_sample(x.size) - 0.5)
+        y = np.ones(n) * 10 + rng.randn(n)
+        x = np.concatenate((x, y))
+
+        truncated, truncated_i = mser.estimate_equilibration_length(x)
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i < n // 2)
+
+        test_passed = True
+
+        try:
+            ucl = mser.ucl(x[truncated_i:])
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertAlmostEqual(mser.mean, 10, places=1)
+
+    def test_N_SKART(self):
+        """Test N_SKART class."""
+        skart = cr.N_SKART()
+
+        self.assertIsNone(skart.indices)
+        self.assertIsNone(skart.si)
+        self.assertIsNone(skart.mean)
+        self.assertIsNone(skart.std)
+
+        x = np.arange(100)
+
+        self.assertRaises(CVGError,
+                          skart.estimate_equilibration_length,
+                          x.reshape(5, 20))
+
+        # input data points are not sufficient
+        self.assertRaises(CVGError, skart.estimate_equilibration_length, x)
+        self.assertRaises(CVGError, skart.ucl, x)
+
+        rng = np.random.RandomState(12345)
+
+        n = 100000
+        x = np.arange(0, 10, 10. / (n / 100))
+        x += (rng.random_sample(x.size) - 0.5)
+        y = np.ones(n) * 10 + rng.randn(n)
+        x = np.concatenate((x, y))
+
+        truncated, truncated_i = skart.estimate_equilibration_length(x)
+
+        self.assertTrue(truncated)
+        self.assertTrue(truncated_i < n // 2)
+
+        test_passed = True
+
+        try:
+            ucl = skart.ucl(x[truncated_i:])
+        except CVGError:
+            test_passed = False
+
+        self.assertTrue(test_passed)
+        self.assertAlmostEqual(skart.mean, 10, places=1)
